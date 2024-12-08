32ef6ea90a34366e00404170656ead8d55b3753a
SHARE IT HUB <156384686+shareithub@users.noreply.github.com>
2024-12-09 01:44:17 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,9 @@ import threading
 import requests
 from fake_useragent import UserAgent
 from concurrent.futures import ThreadPoolExecutor
+from shareithub import HTTPTools, ASCIITools
+
+ASCIITools.print_ascii_intro()
 
 class BotAPI:
     def __init__(self, url):
